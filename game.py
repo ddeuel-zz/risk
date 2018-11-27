1ce9a2cc366ef080d232bfce547242323dcc71b5
Skokowski <skokowski@vpgmhlpt180604f.university.harvard.edu>
2018-11-26 20:30:55 -0500

    game update

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
 import random, pygame, sys, const, pygame.font
 from pygame.locals import *
 
+def enemy_neighbors(player, territory):
+	for p in const.PLAYERS:
+		if p["player"] == player:
+			color = p["color"]
+
+	for neighbor in const.NEIGHBORS:
+		if neighbor["territory"] == territory:
+			neighbors = neighbor["neighbors"]
+
+	enemy_territories = []
+
+	for n in neighbors:
+		if n["color"] != color:
+		enemy_territories.append(n)
+
+	return enemy_territories
