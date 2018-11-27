b200d1e2aeb3c2226dc612fd3f30042a5590a2f0
Drake Deuel <drakedeuel@college.harvard.edu>
2018-11-26 21:19:16 -0500

    updated

--- game.py
+++ game.py
@@ -2,28 +2,102 @@ import random, pygame, sys, const, pygame.font
 from pygame.locals import *
 
 def main():
-  print(hello)
+	print("yeet")
 
-def enemy_neighbors(player, territory):
-  color = None
-  for p in const.PLAYERS:
-    if (p["player"] == player):
-      color = p["color"]
-  if (not color):
-    raise "Error"
+def all_friendly_territories(player):
+	color = None
+
+	for p in const.PLAYERS:
+		if p["player"] == player:
+			color = p["color"]
+
+	if not color:
+		raise "No color"
+
+	friendly_territories = []
+
+	for t in territories:
+		if t["color"] == color:
+			friendly_territories.append(t)
+
+	return friendly_territories
 
 
-  for neighbor in const.NEIGHBORS:
-    if neighbor["territory"] == territory:
-      neighbors = neighbor["neighbors"]
+def specific_friendly_neighbors(territory):
+	color = territory["color"]
 
-  enemy_territories = []
+	for neighbor in const.NEIGHBORS:
+		if neighbor["territory"] == territory:
+			neighbors = neighbor["neighbors"]
 
-  for n in neighbors:
-    if n["color"] != color:
-      enemy_territories.append(n)
+	friendly_territories = []
 
-  return enemy_territories
+	for n in neighbors:
+		if n["color"] == color:
+		  friendy_territories.append(n)
+
+	return friendly_territories
+
+
+def specific_enemy_neighbors(territory):
+	color = territory["color"]
+
+	for neighbor in const.NEIGHBORS:
+		if neighbor["territory"] == territory:
+			neighbors = neighbor["neighbors"]
+
+	enemy_territories = []
+
+	for n in neighbors:
+		if n["color"] != color:
+		  enemy_territories.append(n)
+
+	return enemy_territories
+
+
+def total_enemy_neighbors(player, friendlies):
+	enemy_neighbors = []
+	
+	for f in friendlies:
+		enemy = specific_enemy_neighbors(f)
+		enemy_neighbors.append(enemy_neighbors)
+
+	return enemy_neighbors
+	
+
+def specific_troop_strength(territory):
+	return territory["troops"]
+
+def total_troop_strength(player):
+	territories = all_friendly_territories(player)
+	troops = [specific_troop_strength(t) for t in territories]
+	total_troops = sum(troops)
+
+	return troops
+
+def enemy_territories_in_continent(player, territory):
+	continent = None
+
+	for c in continents:
+		if territory in c["territories"]:
+			continent = c
+
+	color = None
+
+	for p in const.PLAYERS:
+		if p["player"] == player:
+			color = p["color"]
+
+	if not color:
+		raise "No color"
+
+	enemy_territories = 0
+
+	for t in continent:
+		if t["color"] != color
+			enemy_territories += 1
+
+	return enemy_territories
 
 if __name__ == '__main__':
-  main()
+    main()
